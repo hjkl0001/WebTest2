b20d7a2601e9c3df19d64ab6e0b66be6e055193e
BruceVSOL <hjkl0001@outlook.com>
2019-01-16 09:24:01 +0800

    主要内容提交

---
+++ WebTest2/Data/WebTest2Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebTest2.Models
+{
+    public class WebTest2Context : DbContext
+    {
+        public WebTest2Context (DbContextOptions<WebTest2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebTest2.Models.Movie> Movie { get; set; }
+    }
+}
